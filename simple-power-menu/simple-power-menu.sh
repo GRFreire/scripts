6a8321b31233b9172f22f160b26a58e894eb842e
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-08-20 08:13:51 -0300

    simple-power-menu: convert to posix compliant

---
+++ simple-power-menu/simple-power-menu.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ "$(command -v rofi)" ]; then
+    CMD='rofi -dmenu';
+elif [ "$(command -v dmenu)" ]; then
+    CMD='dmenu';
+else
+    echo 'Could not find either dmenu or rofi, exiting'
+    exit 1;
+fi
+
+fn_logout() {
+    case "$DESKTOP_SESSION" in
+        *openbox*) openbox --exit;;
+        *i3*) i3-msg exit;;
+        *qtile*) qtile shell -c 'shutdown()';;
+        *fluxbox*) killall fluxbox;;
+        *bspwm*) bspc quit 1;;
+        *) loginctl terminate-session "${XDG_SESSION_ID-}";;
+    esac
+}
+
+OPTIONS="\
+Shutdown    \tsystemctl poweroff
+Reboot      \tsystemctl reboot
+Suspend     \tsystemctl suspend
+Log out     \tfn_logout"
+
+PROMPT="$(echo "$OPTIONS" | awk -F"\t" '{print $1}')"
+
+PROMPT_LENGHT="$(echo "$PROMPT" | wc -l)"
+
+CHOICE="$(echo "$PROMPT" | $CMD -l "$PROMPT_LENGHT" -p "Power Menu" | xargs)"
+
+if [ -z "$CHOICE" ]; then
+    exit 0;
+fi
+
+COMMAND="$(echo "$OPTIONS" | grep "$CHOICE" | awk -F"\t" '{print $2}')"
+
+if [ -z "$COMMAND" ]; then
+    exit 0;
+fi
+
+CONFIRMATION_CHOICES="no\nyes"
+CONFIRMATION="$(echo $CONFIRMATION_CHOICES | $CMD -l 2 -p "Do you want to $CHOICE")"
+
+if [ -z "$CONFIRMATION" ]; then
+    exit 0;
+fi
+
+if [ "$CONFIRMATION" != yes ]; then
+    exit 0;
+fi
+
+$COMMAND
