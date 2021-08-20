2eb9a11263977d5a08f797128762ed5c0c2827ef
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-08-20 10:29:08 -0300

    vid2mov: create script

---
+++ vid2mov/README.md
@@ -0,0 +1,9 @@
+# vid2mov
+
+## About
+a simple script around ffmpeg to convert a video file to `.mov`.
+
+I personally use this script when I record something in OBS Studio and then want to import into DaVinci Resolve.
+
+## Requirements
+- [ffmpeg](https://www.ffmpeg.org/)
---
+++ vid2mov/vid2mov.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VIDEO_PATH=$1
+[ "$VIDEO_PATH" = "" ] && echo "error: video_path not given" &&  exit 1
+
+[ "$2" != "" ] && OUT_PATH=$2 || OUT_PATH=${VIDEO_PATH%%.*}.mov
+
+if ffmpeg -i "$VIDEO_PATH" -vcodec mpeg4 -q:v 2 -acodec pcm_s16le -q:a 0 -f mov "$OUT_PATH"; then
+    echo "Done"
+    notify-send "vid2mov" "$OUT_PATH done converting"
+    exit 0
+else
+    echo "Failed"
+    notify-send -u critical "vid2mov" "$OUT_PATH error while converting"
+    exit 1
+fi
+
