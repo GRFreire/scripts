80e6474a7c300be9c55f34c0c18994ee1b93b271
Guilherme Rugai Freire <dev@grfreire.com>
2022-03-20 19:13:26 -0300

    create autoxsetwacom script

---
+++ autoxsetwacom/README.md
@@ -0,0 +1,7 @@
+# autoxsetwacom
+
+## About
+List wacom STYLUS device and set its area to the first monitor, while respection the 16/9 aspect ratio
+
+## Requirements
+- xf86-input-wacom
---
+++ autoxsetwacom/autoxsetwacom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# List wacom STYLUS device and set its area to the first monitor, while respection the 16/9 aspect ratio
+
+device_id="$(xsetwacom list devices | grep STYLUS | cut -f2 | cut -d' ' -f2)"
+
+xsetwacom set "$device_id" MapToOutput 1920x1080+0+0
+xsetwacom set "$device_id" Area 0 0 15200 8550
+
