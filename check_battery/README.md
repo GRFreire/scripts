6de54314773fe3cd7ea9b278118c904b8b075a24
Guilherme Rugai Freire <dev@grfreire.com>
2024-09-03 12:07:11 -0300

    add script check_battery

---
+++ check_battery/README.md
@@ -0,0 +1,9 @@
+# check_battery
+
+## About
+Check the battery percentage and notify user if it is about to end.
+
+## Requirements
+ - notify-send
+ - mpv
+ - UPower
---
+++ check_battery/check_battery.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DEVICE="/org/freedesktop/UPower/devices/battery_BAT0"
+
+time="$(upower -i $DEVICE | grep time | cut -d':' -f2 | tr -s ' ' | cut -c2-)"
+perc="$(upower -i $DEVICE | grep percentage | awk '{print $2}' | tr -d '%')"
+stat="$(upower -i $DEVICE | grep state | awk '{print $2}')"
+
+if [ "$perc" -le "20" ]; then
+    urgency="normal"
+    if [ "$perc" -le "10" ]; then
+        urgency="critical"
+    fi
+    notify-send -u "$urgency" --icon="battery" "Battery is low ($perc%)" "Time remaining is $time."
+    mpv /usr/share/sounds/freedesktop/stereo/dialog-information.oga
+fi
+
