3d66555b9bd6bb35b5e09886f62e88b8f233965b
Guilherme Rugai Freire <dev@grfreire.com>
2026-02-11 01:07:34 -0300

    autoxsetwacom: update screen resolution

--- autoxsetwacom/autoxsetwacom.sh
+++ autoxsetwacom/autoxsetwacom.sh
@@ -4,6 +4,6 @@
 
 device_id="$(xsetwacom list devices | grep STYLUS | cut -f2 | cut -d' ' -f2)"
 
-xsetwacom set "$device_id" MapToOutput 1920x1080+0+0
+xsetwacom set "$device_id" MapToOutput 1920x1200+0+0
 xsetwacom set "$device_id" Area 0 0 15200 8550
 
