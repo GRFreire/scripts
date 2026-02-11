2ff33939d2c1d0d7a6fc5dd021bf3125deabbc45
Guilherme Rugai Freire <dev@grfreire.com>
2026-02-11 01:08:13 -0300

    check_battery: disable notification when charging

--- check_battery/check_battery.sh
+++ check_battery/check_battery.sh
@@ -6,6 +6,10 @@ time="$(upower -i $DEVICE | grep time | cut -d':' -f2 | tr -s ' ' | cut -c2-)"
 perc="$(upower -i $DEVICE | grep percentage | awk '{print $2}' | tr -d '%')"
 stat="$(upower -i $DEVICE | grep state | awk '{print $2}')"
 
+if [ "$stat" = "charging" ]; then
+	exit 0
+fi
+
 if [ "$perc" -le "20" ]; then
     urgency="normal"
     if [ "$perc" -le "10" ]; then
